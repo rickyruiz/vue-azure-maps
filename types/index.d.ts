74982d1b5d483f99b4e6aa9ef9892937c9d21c44
Ricardo Ruiz <RRuiz@ebcorp.us>
2019-02-15 11:44:48 -0700

    types(components): 🏷️ add components export

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,6 @@
+import AzureMapControl from '@/plugin/components/AzureMapControl.vue'
+import AzureMapShape from '@/plugin/components/AzureMapShape.vue'
+import AzureMapSymbolLayer from '@/plugin/components/AzureMapSymbolLayer.vue'
 import './vue'
 import { VueAzureMaps } from './vue-azure-maps'
 
@@ -6,3 +9,5 @@ export default VueAzureMaps
 export { atlas } from 'azure-maps-control'
 
 export { VueAzureMapsPluginOptions } from './vue-azure-maps'
+
+export { AzureMapControl, AzureMapSymbolLayer, AzureMapShape }
