f28a48632f8bf81d5a66fcd021b8ec54019d59b0
Ricardo Ruiz <RRuiz@ebcorp.us>
2019-03-27 10:49:02 -0700

    docs(vuepress): 📝 add icons

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -5,6 +5,146 @@ module.exports = {
 
   base: '/vue-azure-maps/',
 
+  head: [
+    [
+      'meta',
+      {
+        charset: 'utf-8',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileColor',
+        content: '#ffffff',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileImage',
+        content: '/img/mstile-150x150.png',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'theme-color',
+        content: '#4DBA87',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '60x60',
+        href: '/img/apple-icon-60x60.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '76x76',
+        href: '/img/apple-icon-76x76.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '120x120',
+        href: '/img/apple-icon-120x120.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '152x152',
+        href: '/img/apple-icon-152x152.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/img/apple-icon-180x180.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '144x144',
+        href: '/img/android-icon-144x144.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/img/android-icon-192x192.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '256x256',
+        href: '/img/android-icon-256x256.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '384x384',
+        href: '/img/android-icon-384x384.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '512x512',
+        href: '/img/android-icon-512x512.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/img/favicon-16x16.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/img/favicon-32x32.png',
+      },
+    ],
+  ],
+
   locales: {
     '/': {
       lang: 'en-US',
@@ -33,7 +173,7 @@ module.exports = {
       '/': {
         label: 'English',
         selectText: 'Languages',
-        editLinkText: 'Edit this page on GitHub',
+        editLinkText: 'Help us improve this page!',
         lastUpdated: 'Last Updated',
         nav: [
           {
