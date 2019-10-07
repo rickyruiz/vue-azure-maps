c2d08a9052936eaf9ef62fdeb52372436dfca527
Ricardo Ruiz <RRuiz@ebcorp.us>
2019-10-07 15:37:57 -0700

    chore(props): 🚧 change prop binding

--- src/plugin/utils/bind-props-to-options.ts
+++ src/plugin/utils/bind-props-to-options.ts
@@ -1,47 +1,53 @@
-import Vue, { WatchHandler, WatchOptions } from 'vue'
+import Vue, { WatchOptions } from 'vue'
 
 export function bindPropsToOptions<K>({
   props,
-  context,
-}: BindPropsToOptionsConfig<K>): K {
+  vm,
+}: BindPropsToOptionsConfig): K {
   let options: Record<string, any> = {}
 
   for (const prop of props) {
-    bindProp(getPropConfig(prop), context)
+    const propConfig = getPropConfig(prop)
+    const { propName, targetPropName = propName } = propConfig
+
+    bindProp(propConfig, vm)
+
+    options[targetPropName] = (vm as any)[propName]
   }
 
   return options as K
 }
 
-export function getPropConfig<T, K>(
-  prop: string | PropToOptionConfig<T, K>
-): PropToOptionConfig<T, K> {
+export function getPropConfig(
+  prop: string | PropToOptionConfig
+): PropToOptionConfig {
   return typeof prop === 'string' ? { propName: prop } : prop
 }
 
-export function bindProp<T, K>(
-  { propName, emittedEventName }: PropToOptionConfig<T, K>,
-  context: Vue
+export function bindProp(
+  {
+    propName,
+    targetPropName = propName,
+    watchHandler = () => {},
+    watchOptions,
+    emittedEventName = `update:${propName}`,
+  }: PropToOptionConfig,
+  vm: Vue
 ): void {
-  if (!emittedEventName) {
-    emittedEventName = `update:${propName}`
-  }
-  // TODO: Create watch handlers to update options
+  vm.$watch(propName, watchHandler, watchOptions)
 }
 
-export interface BindPropsToOptionsConfig<K> {
-  props: (string | PropToOptionConfig<any, K>)[]
-  context: Vue
+export interface BindPropsToOptionsConfig {
+  props: (string | PropToOptionConfig)[]
+  vm: Vue
 }
 
-export interface PropToOptionConfig<T, K> {
+export interface PropToOptionConfig {
   propName: string
   targetPropName?: string
-  target?: any
-  watchHandler?: WatchHandler<T>
+  watchHandler?: (val: any, oldVal: any) => void
   watchOptions?: WatchOptions
-  identity?: (val: T, oldVal: T) => boolean
-  applier?: (val: T, oldVal: T) => void
-  retriever?: () => K
   emittedEventName?: string
 }
+
+export default bindPropsToOptions
