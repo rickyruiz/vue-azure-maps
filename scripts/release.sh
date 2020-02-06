15c629409979952501d8352e6eb735944bf6bafd
Ricardo Ruiz <RRuiz@ebcorp.us>
2020-02-06 10:08:46 -0700

    chore(config): 🔧 update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,13 +6,13 @@ read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Releasing $VERSION ..."
+  echo "Releasing v$VERSION ..."
   # yarn run test
   VERSION=$VERSION yarn run build:lib
 
   # commit
   git add -A
-  git commit -m "chore(build): 📦 $VERSION"
+  git commit -m "build: 📦 bundle $VERSION"
   yarn version --new-version $VERSION --message "chore(release): 🔖 $VERSION"
 
   # publish
