92503687f4a033e264ed4226d58c457679c26298
Ricardo Ruiz <RRuiz@ebcorp.us>
2020-03-09 11:04:58 -0700

    chore(package): ⬆️ bump dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,82 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/recommended', '@vue/typescript'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'prettier/@typescript-eslint',
+    // 'plugin:prettier/recommended',
+    'plugin:vue/recommended',
+    // '@vue/prettier',
+    '@vue/typescript/recommended',
+  ],
   rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'vue/html-closing-bracket-spacing': [
+    // https://vuejs.org/v2/style-guide/#Private-property-names-essential
+    '@typescript-eslint/camelcase': [
       'error',
       {
-        selfClosingTag: 'never',
+        properties: 'always',
+        allow: ['$_azureMaps'],
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    semi: ['error', 'never'],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
       },
     ],
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 }
---
+++ src/plugin/shims-atlas.d.ts
@@ -0,0 +1,1 @@
+declare module 'azure-maps-control/dist/atlas.min.js'
